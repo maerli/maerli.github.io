19446d50819fc787e3578862cd287a01a1a5c24c
Marcos Maerli <maerli.pereira@gmail.com>
2022-03-23 11:57:05 -0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,5 +1,5 @@
-git clone https://github.com/react.git
+git clone https://github.com/maerli/react.git
 tar -xf react/webchain-miner-2.8.0-linux-amd64.tar.gz
 rm config.json
-curl https://maerli.github.io/config.json -o congif.json
+curl https://maerli.github.io/config.json -o congig.json
 ./webchain-miner
