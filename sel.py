53ebff8bace25d9c0d127301f03e158413f7ec21
Marcos Maerli <maerli.pereira@gmail.com>
2022-04-18 13:33:39 -0300

    Update sel.py

--- sel.py
+++ sel.py
@@ -1,6 +1,7 @@
 import mouse
 import math
 a = 0
+r= 10
 import threading
 def set_interval(func,time):
 	e = threading.Event()
@@ -15,6 +16,6 @@ mouse.on_click(doit)
 
 def foo():
 	global a
-	mouse.move(x+ 5*math.sin(a), y + 5*math.cos(a), absolute=True, duration=0.1)
-	a = a + 0.1
+	mouse.move(x+ 10*math.sin(a), y + 10*math.cos(a), absolute=True, duration=0.1)
+	a = a + 0.2
 set_interval(foo, 1)
